6117500dc3f3e21934301098efc6419049ecb274
Verix99 <verix99@seznam.cz>
2023-12-04 15:47:06 +0100

    hotova todo app

--- react-todo-app/react-todo/src/App.jsx
+++ react-todo-app/react-todo/src/App.jsx
@@ -1,33 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react';
+import TaskCard from "./components/TaskCard"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([]); 
+  const [itemName, setItemName] = useState("");
+  const [itemDisc, setItemDisc] = useState("");
+
+ function addName() {
+  if (itemName === "") return alert("Zadejte název úkolu")
+  if (itemDisc === "") return alert("Zadejte popis úkolu")
+
+  const task = {
+    id: Math.floor(Math.random() * 10000),
+    name: itemName,
+    disc: itemDisc
+  }
+  
+  setTasks(oldList => [...oldList, task])
+  setItemName("")
+  setItemDisc("")
+
+  console.log(tasks);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <div className="logo">
+        <h1>TODO App</h1>
+        </div>
+        <div className="login">
+          <h3>LOGIN</h3>
+        </div>
+      </header>
+      <main>
+      <div className="user-wrap">
+            <div className="input-user-name">
+                <input type="text" id="inputName" placeholder="Zadejte název úkolu" value={itemName} onChange={e => setItemName(e.target.value)}/>
+            </div>
+            <div className="input-user-disc">
+                <input type="text" id='inputDisc' placeholder="Zadejte popis úkolu" value={itemDisc} onChange={e => setItemDisc(e.target.value)}/>
+            </div>
+            <div className="btn-wrap">
+                <button className="btn-add" onClick={() => addName()}>Přidat</button>
+            </div>
+        </div>
+        <section className='task-cards'>
+          <ul>
+            {tasks.map(task => {
+              return(
+                <li key={task.id}><TaskCard name={task.name} disc={task.disc}/></li>
+              )
+            })}
+          </ul>
+        </section>
+      </main>
     </>
   )
 }
